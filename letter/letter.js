24a91889390f4c2e28e8e3e5d73642c6d2b17ef2
MisterJokestar <james@marzhillstudios.com>
2025-06-28 17:15:45 -0400

    Made the secret letter look better

--- letter/letter.js
+++ letter/letter.js
@@ -1,32 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const message = `
+const unopenedEnvelopeSrc = "images/pink-envelope.png";
+
+const openedEnvelopeSrc = "images/pink-envelope-open.png";
+
+const message = `
 Dear Kaitlyn,
-<br>I wish for you to know how much I love you.
-<br>So I am sending you this letter, I hope it reaches you well.
-<br>To show my love, I will create an acrostic.
-<br>
-<br>Kaitlyn is the woman I choose to marry.
-<br>Any other fails to compare to her.
-<br>I yearn for the long years we choose to spend with each other.
-<br>The day nears when I will get to declare my commitment before many others.
-<br>Lovingly I will hold her the rest of our days here in this life.
-<br>Young we are now, and old we will become, but the whole time I will be hers.
-<br>Never before was there a girl I wanted to be with like Kaitlyn.
-<br>
-<br>Your loving fiance,
-<br>James
+
+I wish for you to know how much I love you.
+So I am sending you this letter, I hope it reaches you well.
+To show my love, I will create am acrostic.
+
+Kaitlyn is the woman I choose to marry.
+Any other fails to compare to her.
+I yearn for the long years we choose to spend with each other.
+The day nears when I will get to declare my commitment before many others.
+Lovingly I will hold her the rest of our days here in this life.
+Young we are now, and old we will become, but the whole time I will be hers.
+Never before was there a girl I wanted to be with like Kaitlyn.
+
+Your loving fiance,
+James
 `;
 
-  const unopenedEnvelopeSrc = "images/pink-envelope.png";
-
-  const openedEnvelopeSrc = "images/pink-envelope-open.png";
-
+document.addEventListener("DOMContentLoaded", () => {
   const envelope = document.getElementById("letter-envelope");
 
-  const letter = document.getElementById("letter-text");
+  const letterText = document.getElementById("letter-text");
+
+  const letterPage = document.getElementById("letter-contents");
 
   function openLetter() {
-    letter.hidden = false;
+    letterText.innerText = message;
+    letterPage.hidden = false;
     envelope.src = openedEnvelopeSrc;
   }
 
